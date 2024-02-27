b1847ad0c8938f9076047b24427d94afef47d8a7
Michal Charemza <michal@charemza.name>
2024-02-27 17:14:24 +0000

    chore: run database migrations before start
    This is so we can painlessly update MLFlow
    
    (For some reason this wasn't needed on staging - the recent versions upgraded
    fine without this. Have to admit I'm not sure why)

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ set -e
 (
     cd "$(dirname "$0")"
 
+    echo "Running migrations..."
+    mlflow db upgrade $DATABASE_URI
+
     echo "Starting mlflow server and proxy..."
     parallel --will-cite --line-buffer --jobs 2 --halt now,done=1 ::: \
         "mlflow server --host 0.0.0.0 --port 5000 --serve-artifacts --artifacts-destination s3://${ARTIFACT_BUCKET_NAME} --backend-store-uri ${DATABASE_URI} --gunicorn-opts '--log-level debug'" \
